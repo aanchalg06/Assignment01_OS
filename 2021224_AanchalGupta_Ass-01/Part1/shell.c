62b55e8191be348f1e00c4822453fa335db9d3c4
aanchalg06 <108565060+aanchalg06@users.noreply.github.com>
2022-11-17 00:37:03 +0530

    Update shell.c

--- 2021224_AanchalGupta_Ass-01/Part1/shell.c
+++ 2021224_AanchalGupta_Ass-01/Part1/shell.c
@@ -1,306 +1,257 @@
 #include <stdio.h>
 #include <string.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <libgen.h> 
-#include <sys/shm.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <sys/wait.h>
-#include <time.h>
-#include <dirent.h>
-#include <stddef.h>
-#include <stdbool.h>
 
-char* join_str(char* direc,char* join_s){
-    size_t len = strlen(direc);
-    
-    int i=0;
-    while(join_s[i]!='\0'){
-        direc[len+i]= join_s[i];
-        i++;
-    }
-    direc[len+i]=='\0';
-    return direc;
+
+#define INPUT 0
+#define OUTPUT 1
+#define APPEND 2
+
+/*
+removes the newline and space character from the end and start of a char*
+*/
+void removeWhiteSpace(char* buf){
+	if(buf[strlen(buf)-1]==' ' || buf[strlen(buf)-1]=='\n')
+	buf[strlen(buf)-1]='\0';
+	if(buf[0]==' ' || buf[0]=='\n') memmove(buf, buf+1, strlen(buf));
+}
+
+/*
+tokenizes char* buf using the delimiter c, and returns the array of strings in param
+and the size of the array in pointer nr
+*/
+void tokenize_buffer(char** param,int *nr,char *buf,const char *c){
+	char *token;
+	token=strtok(buf,c);
+	int pc=-1;
+	while(token){
+		param[++pc]=malloc(sizeof(token)+1);
+		strcpy(param[pc],token);
+		removeWhiteSpace(param[pc]);
+		token=strtok(NULL,c);
+	}
+	param[++pc]=NULL;
+	*nr=pc;
+}
+
+/*
+used for debugging purposes to print all the strings in a string array
+*/
+void print_params(char ** param){
+	while(*param){
+		printf("param=%s..\n",*param++);
+	}
+}
+
+/*
+loads and executes a single external command
+*/
+void executeBasic(char** argv){
+	if(fork()>0){
+		//parent
+		wait(NULL);
+	}
+	else{
+		//child
+		execvp(argv[0],argv);
+		//in case exec is not successfull, exit
+		perror("invalid input""\n");
+		exit(1);
+	}
+}
+
+/*
+loads and executes a series of external commands that are piped together
+*/
+void executePiped(char** buf,int nr){//can support up to 10 piped commands
+	if(nr>10) return;
+	
+	int fd[10][2],i,pc;
+	char *argv[100];
+
+	for(i=0;i<nr;i++){
+		tokenize_buffer(argv,&pc,buf[i]," ");
+		if(i!=nr-1){
+			if(pipe(fd[i])<0){
+				perror("pipe creating was not successfull\n");
+				return;
+			}
+		}
+		if(fork()==0){//child1
+			if(i!=nr-1){
+				dup2(fd[i][1],1);
+				close(fd[i][0]);
+				close(fd[i][1]);
+			}
+
+			if(i!=0){
+				dup2(fd[i-1][0],0);
+				close(fd[i-1][1]);
+				close(fd[i-1][0]);
+			}
+			execvp(argv[0],argv);
+			perror("invalid input ");
+			exit(1);//in case exec is not successfull, exit
+		}
+		//parent
+		if(i!=0){//second process
+			close(fd[i-1][0]);
+			close(fd[i-1][1]);
+		}
+		wait(NULL);
+	}
+}
+
+/*
+loads and executes a series of external commands that have to be run asyncronously
+*/
+void executeAsync(char** buf,int nr){
+	int i,pc;
+	char *argv[100];
+	for(i=0;i<nr;i++){
+		tokenize_buffer(argv,&pc,buf[i]," ");
+		if(fork()==0){
+			execvp(argv[0],argv);
+			perror("invalid input ");
+			exit(1);//in case exec is not successfull, exit
+		}
+	}
+	for(i=0;i<nr;i++){
+		wait(NULL);
+	}
 
 }
 
-int main(){
-    printf("Shell in C!! Assignment 1 || OS");
-    printf("\n");
+/*
+loads and executes a an external command that needs file redirection(input, output or append)
+*/
+void executeRedirect(char** buf,int nr,int mode){
+	int pc,fd;
+	char *argv[100];
+	removeWhiteSpace(buf[1]);
+	tokenize_buffer(argv,&pc,buf[0]," ");
+	if(fork()==0){
 
-	char cwd_folder[1024];
-    char *directory=getcwd(cwd_folder, sizeof(cwd_folder));
-    if ((directory) != NULL){
-		printf("Present directory(cwd string): %s", cwd_folder);
-        printf("\n");
-    }
-	else {
-    	printf("getcwd failed\n");
-        printf("\n");
-    }
-	
-	char commands_input[1024];
-    printf("%s ",commands_input);
-    printf("\n");
-    bool flag= true;
-	
-	do{
-        printf("Input commands!!");
-        printf("\n");
-		fgets(commands_input,1024,stdin);
-
-		if(commands_input==NULL){
-            printf("no inputs are there hence breaking the loop");
-            printf("\n");
-			break;
+		switch(mode){
+		case INPUT:  fd=open(buf[1],O_RDONLY); break;
+		case OUTPUT: fd=open(buf[1],O_WRONLY); break;
+		case APPEND: fd=open(buf[1],O_WRONLY | O_APPEND); break;
+		default: return;
 		}
 
-		char *internal_command[3]={"echo","cd","pwd"};
-		
-		char new_cwd[1024];
-        char pwd_path[1024];
-		char* sep=" ";
-
-
-		char **ret = malloc(20 * sizeof(char *));
-
-    	char *startp = commands_input, *endp;
-    	size_t l = strlen(sep);
-    	int cnt = 0;
-
-    while (endp = strstr(startp, sep))
-    {
-        ptrdiff_t diff = endp - startp;
-        ret[cnt] = malloc(diff + 1);
-        strncpy(ret[cnt], startp, diff);
-        ret[cnt][diff] = '\0';
-        cnt++;
-        startp = endp + l;
-    }
-
-    if (*startp) // in case last is not a separator
-    {
-        ret[cnt] = malloc(strlen(startp) + 1);
-        strcpy(ret[cnt], startp);
-        cnt++;
-    }
-
-    ret[cnt] = NULL;
-
-    // for(int i=0;i<cnt;i++){
-    //     printf("%s ",ret[i]);
-    //     printf("\n");
-    // }
-       
-		
-		if(strcmp(ret[0],internal_command[0])==0){
-			flag=false;
-
-			if(strcmp(ret[1],"-n")==0){
-                printf("Echo command in process!!");
-                printf("\n");
-                cnt=2;
-                while(ret[cnt]!=NULL){
-                    printf("%s ",ret[cnt]);
-                    printf("\n");
-                    cnt++;
-                }
-                printf("\n");
-			}
-
-			else if(strcmp(ret[1],"-e")==0){
-                printf("Echo command in process!!");
-                printf("\n");
-				cnt=2;
-                while(ret[cnt]!=NULL){
-                    if(ret[cnt]=="\n"){
-                        printf("\n");
-                    }
-                    else{
-                    printf("%s ",ret[cnt]);
-                    printf("\n");
-                    }
-                    cnt++;
-                }
-                printf("\n");
-                
-			}
-
-            else{
-                printf("Some non recognised input recieved !!! try again\n");
-				
-			}
+		if(fd<0){
+			perror("cannot open file\n");
+			return;
 		}
 
-		else if(strcmp(ret[0],internal_command[1])==0){
-			
-			flag=false;
-			if(ret[1]==NULL){
-				char cd_direc[1024];
-    			char *direc=getcwd(cd_direc, sizeof(cd_direc));
-				if(direc==NULL){
-					printf("Error occurred");
-                    printf("\n");
+		switch(mode){
+		case INPUT:  		dup2(fd,0); break;
+		case OUTPUT: 		dup2(fd,1); break;
+		case APPEND: 		dup2(fd,1); break;
+		default: return;
+		}
+		execvp(argv[0],argv);
+		perror("invalid input ");
+		exit(1);//in case exec is not successfull, exit
+	}
+	wait(NULL);
+}
+void command(char** argv, int argc){
+	char* root;
+	char* exec[100];
+			if(strcmp(exec[0],"ls")==0){
+					strcat(root,"/lso");
+					execv(root,exec);
+			}
+
+
+			else if(strcmp(exec[0],"cat")==0){
+					strcat(root,"/cato");
+					execv(root,exec);
+			}
+
+
+			else if(strcmp(exec[0],"rm")==0){
+				if(exec[1]==NULL){
+					printf("Enter a name\n");
 				}
 				else{
-				printf("%s",direc);
-                printf("\n");
-                }
-			}
-			else{
-				char *new_dir;
-
-				if(chdir(ret[1])==-1){
-					printf("Some error occurred\n");
-                    printf("\n");
+					strcat(root,"/rm");
+					execv(root,exec);
 				}
-				else if(chdir(ret[1])==0){
-					new_dir=getcwd(new_cwd,sizeof(new_cwd));
-					printf("New directory is: %s",new_dir);	
-                    printf("\n");				
-				}
-				
 			}
+
+
+			else if(strcmp(exec[0],"date")==0){
+				strcat(root,"/date");
+				execv(root,exec);
+			}
+
+
+			else if(strcmp(exec[0],"mkdir")==0){
+				strcat(root,"/dir");
+				execv(root,exec);
+			}
+}
+
+
+int main(char** argv,int argc)
+{
+	char buf[500],*buffer[100],buf2[500],buf3[500], *params1[100],*params2[100],*token,cwd[1024];
+	int nr=0;
+
+	while(1){
+
+		//print current Directory
+		if (getcwd(cwd, sizeof(cwd)) != NULL)
+		printf("%s  ", cwd);
+		else 	perror("getcwd failed\n");
+
+		//read user input
+		fgets(buf, 500, stdin);//buffer overflow cannot happen
+
+		//check if only a simple command needs to be executed or multiple piped commands or other types
+		if(strchr(buf,'|')){//tokenize pipe commands
+			tokenize_buffer(buffer,&nr,buf,"|");
+			executePiped(buffer,nr);
 		}
-
-		else if(strcmp(ret[0],internal_command[2])==0){
-			flag=false;
-			getcwd(pwd_path,sizeof(pwd_path));
-			printf("Current Directory : %s\n", pwd_path);
-			printf("\n" );
+		else if(strchr(buf,'&')){//asyncronous execution
+			tokenize_buffer(buffer,&nr,buf,"&");
+			executeAsync(buffer,nr);
 		}
-	
-		if(fork()==0){
-			
-			if(strcmp(ret[0],"ls\n")==0){
-                // printf("goodie");
-                printf("\n");
-                char* join_s="/ls_";
-                // char new_cwd_folder[1024];
-                // char *new_directory=getcwd(new_cwd_folder, sizeof(new_cwd_folder));
-                // new_directory= join_str(new_directory,join_s);
-				// execv(new_directory,ret);
-                directory= join_str(directory,join_s);
-				execv(directory,ret);
-			}
-
-            if(strcmp(ret[0],"ls")==0){
-                // printf("1");
-                // printf("\n");
-                // printf("goodie");
-                char* join_s="/ls_";
-                char new_cwd_folder[1024];
-                char *new_directory=getcwd(new_cwd_folder, sizeof(new_cwd_folder));
-                new_directory= join_str(new_directory,join_s);
-				execv(new_directory,ret);
-                directory= join_str(directory,join_s);
-				execv(directory,ret);
-                printf("\n");
-			}
-
-            // else if(strcmp(ret[0],"cat")==0){
-
-            //     if(ret[1]==NULL){
-            //         printf("No file name given\n Renter command with cat filename");
-            //         printf("\n");
-            //     }
-            //     char* join_s="/cat_";
-            //     char new_cwd_folder[1024];
-            //     char *new_directory=getcwd(new_cwd_folder, sizeof(new_cwd_folder));
-            //     new_directory= join_str(new_directory,join_s);
-			// 	// execv(new_directory,ret);
-            //     directory= join_str(directory,join_s);
-			// 	execv(directory,ret);
-			// }
-
-            if(strcmp(ret[0],"cat\n")==0){
-                // printf("goodie");
-                // printf("\n");
-                // char* join_s="/cat_";
-                // char new_cwd_folder[1024];
-                // char *new_directory=getcwd(new_cwd_folder, sizeof(new_cwd_folder));
-                // new_directory= join_str(new_directory,join_s);
-				// execv(new_directory,ret);
-                printf("\n");
-					printf("No file given");
-                    printf("Renter the command with format cat filename");
-                    printf("\n");
-				// }
-                // directory= join_str(directory,join_s);
-				// execv(directory,ret);
-			}
-
-            if(strcmp(ret[0],"cat")==0){
-                // printf("1");
-                // printf("\n");
-                // printf("goodie");
-                char* join_s="/cat_";
-                char new_cwd_folder[1024];
-                char *new_directory=getcwd(new_cwd_folder, sizeof(new_cwd_folder));
-                new_directory= join_str(new_directory,join_s);
-				// execv(new_directory,ret);
-                directory= join_str(directory,join_s);
-				execv(directory,ret);
-                printf("\n");
-			}
-
-            else if(strcmp(ret[0],"date")==0){
-                char* join_s="/date_";
-                directory= join_str(directory,join_s);
-				execv(directory,ret);
-			}
-
-			
-
-			else if(strcmp(ret[0],"rm\n")==0){
-				// if(ret[1]==NULL){
-                    printf("\n");
-					printf("Nothing to remove given");
-                    printf("Renter the command with format rm filename or rm -i filename");
-                    printf("\n");
-				// }
-				// else{
-                    // char* join_s="/rm_";
-                    // char new_cwd_folder[1024];
-                    // char *new_directory=getcwd(new_cwd_folder, sizeof(new_cwd_folder));
-                    // new_directory= join_str(new_directory,join_s);
-				    // execv(new_directory,ret);
-			// }
-            }
-
-            else if(strcmp(ret[0],"rm")==0){
-                char* join_s="/rm_";
-                    // char new_cwd_folder[1024];
-                    // char *new_directory=getcwd(new_cwd_folder, sizeof(new_cwd_folder));
-                    // new_directory= join_str(new_directory,join_s);
-				    // execv(new_directory,ret);
-                directory= join_str(directory,join_s);
-				execv(directory,ret);
-            }
-
-
-			else if(strcmp(ret[0],"mkdir")==0){
-                char* join_s="/mkdir_";
-                directory= join_str(directory,join_s);
-				execv(directory,ret);
-			}
-
-			else {
-				if(flag==true)
-					printf("Command not found\n");
-                    printf("\n");
-			}
-			exit(0);
+		else if(strstr(buf,">>")){//append output to file
+			tokenize_buffer(buffer,&nr,buf,">>");
+			if(nr==2)executeRedirect(buffer,nr,APPEND);
+			else printf("Incorrect output redirection!(has to to be in this form: command >> file)");
 		}
-
-		else{
-			wait(NULL);
+		else if(strchr(buf,'>')){//redirect output to file
+			tokenize_buffer(buffer,&nr,buf,">");
+			if(nr==2)executeRedirect(buffer,nr, OUTPUT);
+			else printf("Incorrect output redirection!(has to to be in this form: command > file)");
+		}
+		else if(strchr(buf,'<')){//redirect file to input
+			tokenize_buffer(buffer,&nr,buf,"<");
+			if(nr==2)executeRedirect(buffer,nr, INPUT);
+			else printf("Incorrect input redirection!(has to to be in this form: command < file)");
+		}
+		else{//single command including internal ones
+			tokenize_buffer(params1,&nr,buf," ");
+			if(strstr(params1[0],"cd")){//cd builtin command
+				chdir(params1[1]);
+			}
+			else if(strstr(params1[0],"exit")){//exit builtin command
+				exit(0);
+			}
+			else executeBasic(params1);
 		}
 	}
-    while(true);
+
 	return 0;
-    
 }
